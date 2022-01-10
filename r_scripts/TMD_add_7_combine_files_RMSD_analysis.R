5a87c82ca8cd9b8746a9462dae4ad07dfc496577
protdoc <KozlovaNastasiaSer@gmail.com>
2022-01-10 09:28:45 +0000

    verified new scripts

--- r_scripts/TMD_add_7_combine_files_RMSD_analysis.R
+++ r_scripts/TMD_add_7_combine_files_RMSD_analysis.R
@@ -2,12 +2,12 @@
 part_start = commandArgs(trailingOnly=TRUE)
 #path_to_PatchDock<-paste0(part_start,"programs/PatchDock/")
 #pach_dock_repeats<-1
-library(dplyr)
-library(bio3d)
-library(readr)
 
 v_RMSD<-10
 
+library(dplyr)
+library(bio3d)
+library(readr)
 part<-paste0(part_start,"structure_prediction/")
 start<-read.pdb(paste0(part_start,"start/start.pdb"))
 df_start<-read.csv(paste0(part_start,"start/df_parts.csv"),stringsAsFactors = F)
@@ -29,39 +29,24 @@ for(w in 1:nrow(df_start)){
 df_start<-df_start%>%filter(!is.na(name))
 
 w<-1
-for(w in 1:nrow(df_start)){
+for (name in 1:nrow(df_start)) {
   part_fin<-paste0(part,df_start$name[w],"/add_domain/",df_start$group_number[w],"/")
   if(!dir.exists(part_fin)){dir.create(part_fin)}
   setwd(part_fin)
   if (!dir.exists("RMSD/")){dir.create("RMSD/")}
-  df_RMSD<-read.csv("df_interactions_fin.csv",stringsAsFactors = F)
-  df_RMSD<-df_RMSD%>%mutate(RMSD=NA)
-  df_RMSD<-df_RMSD%>%select(number.x,number.y,RMSD)
-  v_model<-unique(df_RMSD$number.x)
-  b<-c()
-  for (j in 1:length(v_model)) {
-    if (!file.exists(paste0("RMSD/",v_model[j],".csv")) ){
-      b<-c(b,v_model[j])
+#  print(paste0("start ",Sys.time()))
+  v_RMSD_files<-list.files(paste0("RMSD/"))
+  if(length(v_RMSD_files)>0){
+    df_RMSD<-read.csv(paste0("RMSD/",v_RMSD_files[1]),stringsAsFactors = F)
+    df_RMSD<-df_RMSD%>%filter(RMSD<v_RMSD)
+    for (i in 2:length(v_RMSD_files)) {
+      df_RMSD_add<-read.csv(paste0("RMSD/",v_RMSD_files[i]),stringsAsFactors = F)
+      df_RMSD_add<-df_RMSD_add%>%filter(RMSD<v_RMSD)
+      df_RMSD<-rbind(df_RMSD,df_RMSD_add)
+      
     }
+    print(paste0("finish ",Sys.time()))
+    write.csv(df_RMSD,"df_RMSD_all.csv",row.names = F)
   }
-  v_model<-b
-  v_model<-sort(v_model)
-  df_RMSD_TEMP<-df_RMSD
-  df_RMSD<-df_RMSD[df_RMSD$number.x%in%v_model,]
-  if(length(v_model)>0){
-    for (j in 1:length(v_model)) {
-      df_RMSD_TEMP<-  df_RMSD%>%filter(number.x==v_model[j])
-      pdb_1<-read.pdb(paste0("structure/out.txt.",v_model[j],".pdb"))
-      pdb_1.int<-atom.select(pdb_1, "calpha")
-      for (i in 1:nrow(df_RMSD_TEMP)) {
-        pdb_2<-read.pdb(paste0("structure/out.txt.",df_RMSD_TEMP$number.y[i],".pdb"))
-        pdb_2.int<-atom.select(pdb_2, "calpha")
-        df_RMSD_TEMP$RMSD[i]<-rmsd(a=pdb_1,b=pdb_2,a.inds = pdb_1.int,b.inds = pdb_2.int,fit=T)
-  
-      }
-      df_RMSD_TEMP<-df_RMSD_TEMP%>%filter(RMSD<10)
-      write.csv(df_RMSD_TEMP,paste0("RMSD/",v_model[j],".csv"),row.names = F)
-      df_RMSD<-  df_RMSD%>%filter(number.x!=v_model[j])
-    }
-  }
+#  print(paste0("start ",Sys.time()))
 }
