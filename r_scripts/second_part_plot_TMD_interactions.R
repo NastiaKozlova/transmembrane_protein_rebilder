9e8c4f93cced396bdb296ffefc892c29f37c5df7
NastiaKozlova <kozlovaas.93gmail.com>
2024-07-31 13:11:14 -0500

    make plot of TMD interactions

--- r_scripts/second_part_plot_TMD_interactions.R
+++ r_scripts/second_part_plot_TMD_interactions.R
@@ -4,23 +4,20 @@ library(bio3d)
 library(ggplot2)
 library(dplyr)
 library(readr)
-
-
-library(GGally)
-library(network)
+library(cowplot)
+library(magick)
+library(igraph)
+#library(network)
 setwd(part_start)
 df_start<-read.csv("results/df_second_part.csv",stringsAsFactors = F)
-df_start<-df_start%>%mutate(group_number=paste0("first_part_",first_part_group_number,"_second_part_",second_part_group_number))
 df_topology<-read.csv("start/df_topology.csv",stringsAsFactors = F)
-df_seq<-data.frame(matrix(nrow = max(c(df_start$first_part_finish,df_start$second_part_finish,df_start$third_part_finish)),ncol = 2))
+df_seq<-data.frame(matrix(nrow = max(c(df_start$second_part_finish,df_start$second_part_finish)),ncol = 2))
 colnames(df_seq)<-c("amino","TMD")
 df_seq$amino<-c(1:nrow(df_seq))
 df_topology<-df_topology%>%filter(type=="Transmembrane")
-
 df_topology<-df_topology%>%mutate(type=c(1:nrow(df_topology)))
-i<-1
 for (i in 1:nrow(df_topology)) {
-  df_seq$TMD[df_seq$amino>=df_topology$seq_beg[i]&df_seq$amino<=df_topology$seq_end[i]]<-df_topology$type[i]
+    df_seq$TMD[df_seq$amino>=df_topology$seq_beg[i]&df_seq$amino<=df_topology$seq_end[i]]<-df_topology$type[i]
 }
 df_seq<-df_seq%>%filter(!is.na(TMD))
 if (!dir.exists(paste0(part_start,"results/second_part/interactions/"))){dir.create(paste0(part_start,"results/second_part/interactions/"))}
@@ -28,129 +25,99 @@ if (!dir.exists(paste0(part_start,"results/second_part/ring2/"))){dir.create(pas
 if (!dir.exists(paste0(part_start,"results/second_part/ring/"))){dir.create(paste0(part_start,"results/second_part/ring/"))}
 if (!dir.exists(paste0(part_start,"results/second_part/interactions/"))){dir.create(paste0(part_start,"results/second_part/interactions/"))}
 if (!dir.exists(paste0(part_start,"results/second_part/TMD_interactions/"))){dir.create(paste0(part_start,"results/second_part/TMD_interactions/"))}
-if (!dir.exists(paste0(part_start,"results/second_part/plot/"))){dir.create(paste0(part_start,"results/second_part/plot/"))}
-if (!dir.exists(paste0(part_start,"results/second_part/plot/TMD_interactions"))){dir.create(paste0(part_start,"results/second_part/plot/TMD_interactions"))}
-if (!dir.exists(paste0(part_start,"results/second_part/plot/interactions"))){dir.create(paste0(part_start,"results/second_part/plot/interactions"))}
+if (!dir.exists(paste0(part_start,"results/second_part/TMD_interactions_plot/"))){dir.create(paste0(part_start,"results/second_part/TMD_interactions_plot/"))}
+
+if (!dir.exists(paste0(part_start,"results/second_part/plot_merge/"))){dir.create(paste0(part_start,"results/second_part/plot_merge/"))}
+if (!dir.exists(paste0(part_start,"results/second_part/plot_arrange/"))){dir.create(paste0(part_start,"results/second_part/plot_arrange/"))}
+
 i<-1
 
 
 part<-paste0(part_start,"results/second_part/")
 setwd(part)
-i<-3
-for (i in 1:nrow(df_start)) {
-  
-  df_ring<-read.csv(paste0("TMD_interactions/",df_start$name[i],"_",df_start$group_number[i],".txt"),stringsAsFactors = F)
-  df_ring<-df_ring%>%mutate(TMD_1=paste0("TM",TMD_1))
-  df_ring<-df_ring%>%mutate(TMD_2=paste0("TM",TMD_2))
-  df_ring<-df_ring%>%filter(TMD_1!=TMD_2)
-  df_pdb1<-df_ring%>%select(TMD_1, TMD_2, bond,           bonds_quantity)
-  df_pdb2<-df_ring%>%select(TMD_2, TMD_1, bond,           bonds_quantity)
-  colnames(df_pdb2)<-colnames(df_pdb1)
-  df_pdb<-rbind(df_pdb1,df_pdb2)
-  df_pdb<-df_pdb%>%select(TMD_1)
-  df_pdb<-unique(df_pdb)
-#  df_pdb<-df_pdb%>%mutate(type=paste0("TM",TMD_1))
-  df_pdb<-unique(df_pdb)
-  if(nrow(df_ring)>1){
-    vlist<-list(edges =df_ring,vertices =df_pdb)
-    
-    rownames(vlist$vertices) <- vlist$vertices$TMD_1
-    mm.net <- network(vlist$edges[, 1:4], directed = FALSE)
-    mm.net %v% "type" <- as.character(
-      vlist$vertices[ network.vertex.names(mm.net), "type"]
-    )
-    # type color palette
-    mm.col <- c("EMD1"= "#ff69b4",
-                "TM1"= "#4783B7",
-                "EMD2" = "#ff69b4", 
-                "loop1"= "#80B682",
-                "EMD3" = "#ff69b4",
-                "TM2" = "#4783B7",
-                "EMD4"  = "#ff69b4",
-                "TM3"  = "#4783B7",
-                "EMD5" = "#ff69b4",
-                "TM4" = "#4783B7",
-                "EMD6" = "#ff69b4",
-                "loop2" = "#80B682",
-                "EMD7"  = "#ff69b4",
-                "TM5" = "#4783B7",
-                "EMD8"= "#ff69b4",
-                "TM6" = "#4783B7",
-                "EMD9" = "#ff69b4",
-                "TM7"  = "#4783B7",
-                "TM8"  = "#4783B7",
-                "EMD10"= "#ff69b4",
-                "EMD11"= "#ff69b4",
-                "EMD12"= "#ff69b4")
-    # create plot for ggnet2
-    set.seed(10052006)
-    p<-ggnet2(mm.net, #edge.color = mm.col[ mm.net %v% "type" ],
-              label = T,
-              #  node.label = "type",
-              # labelon = TRUE, 
-              #     label.color = mm.col[ mm.net %v% "type" ],
-              edge.size = "bonds_quantity",
-              size = 5, vjust = -0.6, mode = "kamadakawai", label.size = 5)
-    #ggsave(p,filename=paste0("TMD_interactions.png"), width = 20, height = 20, units = c("cm"), dpi = 200 ) 
-    ggsave(p,filename = paste0("plot/TMD_interactions/",df_start$name[i],"_",df_start$group_number[i],".png"), width = 20, height = 20, units = c("cm"), dpi = 200 ) 
-  }
-}
 i<-1
+df_start<-df_start%>%mutate(group_number=paste0("first_part_",first_part_group_number,"_second_part_",second_part_group_number))
+
 for (i in 1:nrow(df_start)) {
-  
-  df_ring<-read.csv(paste0("interactions/",df_start$name[i],"_",df_start$group_number[i],".txt"),stringsAsFactors = F)
-  df_ring<-df_ring%>%filter(abs(NodeId1-NodeId2)>5)
-  df_ring<-df_ring%>%mutate(Node_1=paste(resid_1,NodeId1))
-  df_ring<-df_ring%>%mutate(Node_2=paste(resid_2,NodeId2))
-  df_ring<-df_ring%>%select(Node_1,Node_2)
-  df_ring<-unique(df_ring)
-  pdb<-read.pdb(paste0("structure/",df_start$name[i],"/",df_start$group_number[i],".pdb"))
-  df_pdb<-pdb$atom
-  df_pdb<-df_pdb%>%select(resid,resno)
-  df_pdb<-unique(df_pdb)
-  df_pdb<-df_pdb%>%mutate(type=paste(resid,resno))
-  df_pdb<-df_pdb%>%select(type)
-  if(nrow(df_ring)>1){
-    vlist<-list(edges =df_ring,vertices =df_pdb)
     
-    rownames(vlist$vertices) <- vlist$vertices$type
-    mm.net <- network(vlist$edges[, 1:2], directed = FALSE)
-    mm.net %v% "type" <- as.character(
-      vlist$vertices[ network.vertex.names(mm.net), "type"]
+    df_ring<-read.csv(paste0("TMD_interactions/",df_start$name[i],"_",df_start$group_number[i],".txt"),stringsAsFactors = F)
+    df_ring<-df_ring%>%filter(TMD_1!=TMD_2)
+    
+    df_ring<-df_ring%>%select(TMD_1,TMD_2,bonds_quantity)
+    
+    
+    df_pdb1<-df_ring%>%select(TMD_1)
+    df_pdb2<-df_ring%>%select(TMD_2)
+    colnames(df_pdb2)<-colnames(df_pdb1)
+    df_pdb<-rbind(df_pdb1,df_pdb2)
+    df_pdb<-unique(df_pdb)
+    df_pdb<-df_pdb%>%mutate(part="first")
+    
+    df_pdb$part[as.numeric(df_pdb$TMD_1)>6]<-"second"
+    df_pdb$part[as.numeric(df_pdb$TMD_1)==7]<-"TM7"
+    df_pdb$part[is.na(as.numeric(df_pdb$TMD_1))]<-"not TM"
+    df_pdb$part<-as.factor(df_pdb$part)
+    
+    g<-graph_from_data_frame(d = df_ring, vertices = df_pdb,
+                             directed = FALSE)#%%>%
+    g <- set_vertex_attr(g, 'color' , value = df_pdb$part)
+    g <- set_edge_attr(g, 'bonds' , value = df_ring$bonds_quantity)
+    png(paste0("TMD_interactions_plot/",df_start$name[i],"_",df_start$group_number[i],".png"), width = 1000, height = 1000)
+    
+    plot.igraph(g,
+                edge.width = E(g)$bonds,
+                vertex.label.color = "black",
+                size=100,
+                layout = layout_nicely(g),
+                vertex.color = V(g)$color,
     )
-    # type color palette
-    mm.col <- c("EMD1"= "#ff69b4",
-                "TM1"= "#4783B7",
-                "EMD2" = "#ff69b4", 
-                "loop1"= "#80B682",
-                "EMD3" = "#ff69b4",
-                "TM2" = "#4783B7",
-                "EMD4"  = "#ff69b4",
-                "TM3"  = "#4783B7",
-                "EMD5" = "#ff69b4",
-                "TM4" = "#4783B7",
-                "EMD6" = "#ff69b4",
-                "loop2" = "#80B682",
-                "EMD7"  = "#ff69b4",
-                "TM5" = "#4783B7",
-                "EMD8"= "#ff69b4",
-                "TM6" = "#4783B7",
-                "EMD9" = "#ff69b4",
-                "TM7"  = "#4783B7",
-                "TM8"  = "#4783B7",
-                "EMD10"= "#ff69b4",
-                "EMD11"= "#ff69b4",
-                "EMD12"= "#ff69b4")
-    # create plot for ggnet2
-    set.seed(10052006)
-    p<-ggnet2(mm.net, #edge.color = mm.col[ mm.net %v% "type" ],
-              label = T,
-              #  node.label = "type",
-              # labelon = TRUE, 
-              #     label.color = mm.col[ mm.net %v% "type" ],
-              #edge.size = "bonds_quantity",
-              size = 5, vjust = -0.6, mode = "kamadakawai", label.size = 5)
-    #ggsave(p,filename=paste0("TMD_interactions.png"), width = 20, height = 20, units = c("cm"), dpi = 200 ) 
-    ggsave(p,filename = paste0("plot/interactions/",df_start$name[i],"_",df_start$group_number[i],".png"), width = 20, height = 20, units = c("cm"), dpi = 200 ) 
-  }
+    dev.off()  
 }
+for (i in 1:nrow(df_start)) {
+    values <- as.numeric(factor(V(g)$color))
+    
+    # Calculate the observed assortativity
+    observed.assortativity <- assortativity(g, values)
+    # Calculate the assortativity of the network randomizing the gender attribute 1000 times
+    results<-100000
+    v_assortativity <- vector('list', results)
+    #  v_assortativity.degree <- vector('list', 100000)
+    for(j in 1:results){
+        v_assortativity[[j]] <- assortativity(g, sample(values),directed = F)
+        #    v_assortativity.degree[[j]] <- assortativity.degree(g, directed = FALSE)
+    }
+    v_scale_x<-seq(from=-1,to=1,by=0.1)
+    # Plot the distribution of assortativity values and add a red vertical line at the original observed value
+    v_assortativity<-(unlist(v_assortativity))
+    #  v_assortativity.degree<-(unlist(v_assortativity.degree))
+    df_results<-data.frame(matrix(nrow=results,ncol=2))
+    colnames(df_results)<-c("assortativity","assortativity.degree")
+    df_results$assortativity<-v_assortativity
+    #  df_results$assortativity.degree<-v_assortativity.degree
+    p_assortativity<-ggplot(df_results)+
+        geom_density(aes(x=assortativity))+#,after_stat(density)),binwidth=0.05)+
+        geom_vline(xintercept=observed.assortativity, linetype="dashed", color = "red")+
+        theme_bw()+
+        scale_x_continuous(breaks=v_scale_x,labels=v_scale_x)
+    
+    TMD_interaction<-paste0(part,"TMD_interactions_plot/",df_start$name[i],"_",df_start$group_number[i],".png")
+    TMD_pictures<-paste0(part,"pictures/",df_start$name[i],"_",df_start$group_number[i],".png")
+    #file.exists(file_name)
+    #logo_file <- system.file( file_name, package = "cowplot")
+    
+    p_TMD_interaction<-ggdraw() +  
+        draw_image(TMD_interaction, scale = 1)
+    p_TMD_pictures<-ggdraw() +  
+        draw_image(TMD_pictures, scale = 1)
+    
+    p1<-plot_grid(p_TMD_interaction, p_TMD_pictures, label_size = 12,ncol=2)
+    merge_plot<-plot_grid(p_assortativity, p1,  label_size = 12,ncol=1)+
+        theme(plot.background = element_rect(fill = "white", colour = "white"))
+    ggsave(merge_plot,filename = paste0(part,"plot_merge/",df_start$name[i],"_",df_start$group_number[i],".png"), width = 20, height = 20, units = c("cm"), dpi = 200 )
+    p_arrange<-plot_grid(p_TMD_interaction, p_TMD_pictures, label_size = 12,ncol=2)+
+        theme(plot.background = element_rect(fill = "white", colour = "white"))
+    
+    ggsave(p_arrange,filename = paste0(part,"plot_arrange/",df_start$name[i],"_",df_start$group_number[i],".png"), width = 20, height = 10, units = c("cm"), dpi = 200 )
+    
+}
+
