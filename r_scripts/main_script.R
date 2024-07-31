c72a00f2636e620c25c107e641486378d3c83dc6
NastiaKozlova <kozlovaas.93gmail.com>
2024-07-31 13:24:24 -0500

    rearrange scripts

--- r_scripts/main_script.R
+++ r_scripts/main_script.R
@@ -38,16 +38,18 @@ system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/first_part_d
 system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/first_part_TMD_interactions.R ",part_start),ignore.stdout=T,wait = T)
 
 
-#TMD orinetation picture
-system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/first_part_TMD_orientation_picture.R ",part_start),ignore.stdout=T,wait = T)
 #make interactions plot 
 #need pictures
-system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/plot_TMD_interactions.R ",part_start),ignore.stdout=T,wait = T)
+
+system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/first_part_plot_TMD_interactions.R ",part_start),ignore.stdout=T,wait = T)
+
+
+#TMD orinetation picture
+system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/first_part_TMD_orientation_picture.R ",part_start),ignore.stdout=T,wait = T)
 
 #VMD run and make pictures
 
-#first_part_TMD_orientation_picture
-#system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/first_part_plot_TMD_interactions.R ",part_start),ignore.stdout=T,wait = T)
+system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/first_part_plot_TMD_interactions.R ",part_start),ignore.stdout=T,wait = T)
 
 
 #prepare pdb and run PatchDOCK second time
@@ -77,6 +79,8 @@ system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/second_part_
 #calculate interprotein interaction using Ring2 and interactions between domains based on Ring2 data
 #will give an error, make manually
 system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/second_part_TMD_interactions.R ",part_start),ignore.stdout=T,wait = T)
+system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/second_part_plot_TMD_interactions.R ",part_start),ignore.stdout=T,wait = T)
+system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/second_part_TMD_orientation_picture.R ",part_start),ignore.stdout=T,wait = T)
 system(command = paste0("Rscript --vanilla  ",part_start,"r_scripts/second_part_plot_TMD_interactions.R ",part_start),ignore.stdout=T,wait = T)
 
 #count control (starting structure parameters)
