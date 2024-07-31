19637a8ca801586777ff34efee98890d7f5a7ebb
NastiaKozlova <kozlovaas.93gmail.com>
2024-07-31 15:35:55 -0500

    fix script

--- r_scripts/first_part_merge_pictures.R
+++ r_scripts/first_part_merge_pictures.R
@@ -42,6 +42,39 @@ part<-paste0(part_start,"results/first_part/")
 setwd(part)
 i<-1
 for (i in 1:nrow(df_start)) {
+    
+    df_ring<-read.csv(paste0("TMD_interactions/",df_start$name[i],"_",df_start$group_number[i],".txt"),stringsAsFactors = F)
+    df_ring<-df_ring%>%filter(TMD_1!=TMD_2)
+    
+    df_ring<-df_ring%>%select(TMD_1,TMD_2,bonds_quantity)
+    
+    
+    df_pdb1<-df_ring%>%select(TMD_1)
+    df_pdb2<-df_ring%>%select(TMD_2)
+    colnames(df_pdb2)<-colnames(df_pdb1)
+    df_pdb<-rbind(df_pdb1,df_pdb2)
+    df_pdb<-unique(df_pdb)
+    df_pdb<-df_pdb%>%mutate(part="first")
+    
+    df_pdb$part[as.numeric(df_pdb$TMD_1)>6]<-"second"
+    df_pdb$part[as.numeric(df_pdb$TMD_1)==7]<-"TM7"
+    df_pdb$part[is.na(as.numeric(df_pdb$TMD_1))]<-"not TM"
+    df_pdb$part<-as.factor(df_pdb$part)
+    
+    g<-graph_from_data_frame(d = df_ring, vertices = df_pdb,
+                             directed = FALSE)#%%>%
+    g <- set_vertex_attr(g, 'color' , value = df_pdb$part)
+    g <- set_edge_attr(g, 'bonds' , value = df_ring$bonds_quantity)
+    png(paste0("TMD_interactions_plot/",df_start$name[i],"_",df_start$group_number[i],".png"), width = 1000, height = 1000)
+    
+    plot.igraph(g,
+                edge.width = E(g)$bonds,
+                vertex.label.color = "black",
+                size=100,
+                layout = layout_nicely(g),
+                vertex.color = V(g)$color,
+    )
+    dev.off()  
     values <- as.numeric(factor(V(g)$color))
     
     # Calculate the observed assortativity
